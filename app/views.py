4d54de50d978e33b46744bdaaeb8a476cee4b5c9
kymone876 <55679391+kymone876@users.noreply.github.com>
2021-04-11 13:57:38 -0500

    created upload function

--- app/views.py
+++ app/views.py
@@ -6,8 +6,10 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request
-
+from flask import render_template, request, jsonify
+from .forms import UploadForm
+from werkzeug.utils import secure_filename
+import os
 ###
 # Routing for your application.
 ###
@@ -27,6 +29,22 @@ def index(path):
     Also we will render the initial webpage and then let VueJS take control.
     """
     return render_template('index.html')
+
+@app.route("/api/upload", methods=["POST"])
+def uploads():
+    form = UploadForm()
+    if form.validate_on_submit():
+        description = form.description.data
+        photoData = form.photo.data
+        filename = secure_filename(photoData.filename)
+        try:
+            photoData.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return jsonify(message="File Upload Successful", filename=filename, description=description)
+        except Exception as e:
+            print(e)
+            return jsonify(errors=["internal Error: something worny with the photo you uploaded"])
+    return jsonify(errors=form_errors(form))
+
 
 
 # Here we define a function to collect form errors from Flask-WTF
